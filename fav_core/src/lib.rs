2d80caea1fe9436bb1bc5ae3ff517144013a5e5e
Louis <836250617@qq.com>
2024-06-17 03:47:29 +0800

    fix: docsrs doc_auto_cfg

--- fav_core/src/lib.rs
+++ fav_core/src/lib.rs
@@ -1,6 +1,6 @@
 #![doc = include_str!("../README.md")]
 #![deny(missing_docs, rustdoc::broken_intra_doc_links)]
-#![cfg_attr(docrs, feature(doc_auto_cfg))]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
 
 pub mod api;
 pub mod attr;
